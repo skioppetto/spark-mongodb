88205e43dd919890cbcbdf1e1f3340b378640da5
Alberto Coppini <alberto.coppini@valbruna.it>
2015-10-30 13:52:17 +0100

    added service mothods for crud operations

--- src/main/java/ac/test/service/AppService.java
+++ src/main/java/ac/test/service/AppService.java
@@ -1,7 +1,17 @@
 package ac.test.service;
 
+import ac.test.model.Pojo;
+
 public interface AppService {
 
 	public String hello();
 
+	public Pojo get(String id);
+
+	public String delete(String id);
+
+	public Pojo update(Pojo pojo);
+
+	public Pojo create(Pojo pojo);
+	
 }
--- src/main/java/ac/test/service/AppServiceImpl.java
+++ src/main/java/ac/test/service/AppServiceImpl.java
@@ -1,8 +1,39 @@
 package ac.test.service;
 
+import ac.test.model.Pojo;
+import ac.test.model.PojoRepository;
+
+import com.google.inject.Inject;
+
 public class AppServiceImpl implements AppService{
 
+	@Inject
+	private PojoRepository repository;
+	
+	
 	@Override
 	public String hello(){return "Hello World";}
+
+	@Override
+	public Pojo get(String id) {
+		return repository.get(id);
+	}
+	
+	@Override
+	public Pojo create(Pojo pojo) {
+		return repository.createOrUpdate(pojo);
+	}
+	
+	@Override
+	public Pojo update(Pojo pojo) {
+		return repository.createOrUpdate(pojo);
+	}
+	
+	@Override
+	public String delete(String id) {
+		return (repository.delete(id))?"{result:\"success\"}":"{result:\"failure\"}";
+	}
+	
+	
 	
 }
